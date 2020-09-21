bbda7858e9f74905847da35ef7caa606dca7ceaf
alexombre <moutapamalexandre@gmail.com>
2020-09-21 23:24:37 +0200

    navbar change

--- src/components/App/Navbar.jsx
+++ src/components/App/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+  
+  let locationChecker = useLocation().pathname.split('/')[1]!="";
   
   return (
     <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-primary shadow">
@@ -11,12 +13,17 @@ const Navbar = () => {
       </button>
       <div class="collapse navbar-collapse align-self-start justify-content-start" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link text-white" href="#stack">Stack</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="#competences">Compétences</a>
-          </li>
+          { !locationChecker &&
+            <>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#stack">Stack</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#competences">Compétences</a>
+            </li>
+            </>
+          }
+          
         </ul>     
       </div>
       <div class="collapse navbar-collapse align-self-end justify-content-end" id="navbarNav">
